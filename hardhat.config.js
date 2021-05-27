8fc812ab8ddc9e9b04b60a2a4657e99179c769c9
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-27 15:40:27 +0200

    contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-solhint');
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: '0.8.4',
 };
